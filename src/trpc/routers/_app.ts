bd0ee0663afe6a0ec9360f63703c41947064f7c9
Kuldeep <kunalrajput67718@gmail.com>
2025-10-09 22:46:47 +0530

    trpc configuration , enable transformer context,add auth to trpc context, add protectdprocedurs, add rate limiting by upstash

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,17 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from "zod";
+import {  baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
-      }),
+      })
     )
     .query((opts) => {
-     
+      console.log("trpc console");
+      console.log({ fromContext: opts.ctx.user });
+
       return {
         greeting: `hello ${opts.input.text}`,
       };
