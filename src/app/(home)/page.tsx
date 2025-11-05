8430dfa52d3379d2421a17aec9a7185abd69d429
Kuldeep <kunalrajput67718@gmail.com>
2025-11-05 23:06:10 +0530

    sidebar and navbar done

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Home = () => {
+  return <div>I will upload video in future</div>;
+};
+
+export default Home;
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const Home = () => {
-  return <div>
-    <Button>Tab</Button>
-  </div>;
-};
-
-export default Home;
