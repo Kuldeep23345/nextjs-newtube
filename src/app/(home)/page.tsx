97578ff6708e9db6ad0102543ee1b9145b86dba3
Kuldeep <kunalrajput67718@gmail.com>
2025-11-08 00:15:06 +0530

    added video categories carousel

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,19 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
-import { PageClient } from "./client";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+export const dynamic = "force-dynamic";
 
-export default async function Home() {
-  void trpc.hello.prefetch({ text: "i am here to learn" });
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>;
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const { categoryId } = await searchParams;
+  void trpc.categroies.getMany.prefetch();
   return (
     <HydrateClient>
-      <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <PageClient />
-        </Suspense>
-      </ErrorBoundary>
+      <HomeView categoryId={categoryId}/>
     </HydrateClient>
   );
 }
